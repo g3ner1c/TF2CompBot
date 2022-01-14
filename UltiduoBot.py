4436858532025bc4045a6ebd61ba38be2e4ab458
Rocky <74469257+awesomeplaya211@users.noreply.github.com>
2022-01-13 20:32:35 -0500

    removed messy local stuff
    jsons and player ids to make it more universal

--- UltiduoBot.py
+++ UltiduoBot.py
@@ -34,15 +34,15 @@ bot.help_command = PrettyHelp(color=0xcf7336) # 0xcf7336 should be used for all 
 # client = discord.Client()
 slash = SlashCommand(bot, sync_commands=True)
 
-with open("jsons/discord2steamid.json",'r') as f:
+# with open("jsons/discord2steamid.json",'r') as f:
 
-	discord2steam = json.load(f)
-	f.close()
+# 	discord2steam = json.load(f)
+# 	f.close()
 
-with open("jsons/hours.json",'r') as f:
+# with open("jsons/hours.json",'r') as f:
 
-	hours = json.load(f)
-	f.close()
+# 	hours = json.load(f)
+# 	f.close()
 
 
 async def heartbeat():
@@ -68,241 +68,241 @@ async def heartbeat():
 		await asyncio.sleep(40)
 		
 
-async def check_hours_played():
-
-	await bot.wait_until_ready()
-	while not bot.is_closed():
-
-		global hours
-		global discord2steam
-		
-		with open("jsons/discord2steamid.json",'r') as f:
-
-			discord2steam = json.load(f)
-			f.close()
-
-
-		for user_id in list(discord2steam):
-
-
-			url = f"https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={os.getenv('steamtoken')}&input_json="+quote(f'{{"steamid": {discord2steam[user_id]}, "include_played_free_games": true, "appids_filter": [440]}}')
-
-			r = requests.get(url)
-
-			try:
-
-				hours_played = json.loads(r.text)['response']['games'][0]['playtime_forever']/60
-				
-				# print(user_id + ': ' + discord2steam[user_id] + ': ' + str(hours_played))
-
-				try:
-
-					if (hours_played // 100) > (hours[user_id] // 100):
-					
-						embed=discord.Embed(title="Congratulations!", description=f"<@{user_id}> has reached **{str(int((hours_played // 100)*100))} hours** in TF2!", color=0xcf7336)
-
-						embed.timestamp = datetime.datetime.utcnow()
-
-						await bot.get_channel(890820026800676894).send(embed=embed)
-				
-				except KeyError: # New entry in dict
-
-					pass
-
-				hours[user_id] = hours_played
-
-			except KeyError:
-
-				pass
-
-		print('hours updated')
-
-		with open("jsons/hours.json", 'w') as f:
-
-			json.dump(hours, f, indent=4)
-			f.close()
-
-		with open("jsons/hours.json", 'r') as f:
-			
-			hours = json.load(f)
-			f.close()
-			
-
-		await asyncio.sleep(300) # checks every 5 minutes
-
-# async def vendor_period():
+# async def check_hours_played():
 
 # 	await bot.wait_until_ready()
 # 	while not bot.is_closed():
 
-# 		await bot.get_channel(920551455868477460).send(". - randomdotvendor")
-# 		await asyncio.sleep(random.randint(1,30)) # checks every 5 minutes
+# 		global hours
+# 		global discord2steam
+		
+# 		with open("jsons/discord2steamid.json",'r') as f:
+
+# 			discord2steam = json.load(f)
+# 			f.close()
 
 
-playingStatus = ['Ultiduos', 'Spire MGE', 'Uncletopia | Atlanta 1', '24/7 plr_hightower'
-				 ]
+# 		for user_id in list(discord2steam):
 
 
-users = list(discord2steam)
+# 			url = f"https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={os.getenv('steamtoken')}&input_json="+quote(f'{{"steamid": {discord2steam[user_id]}, "include_played_free_games": true, "appids_filter": [440]}}')
 
+# 			r = requests.get(url)
 
-@bot.event
-async def on_ready():
+# 			try:
 
-	print('Logged in as {0.user}'.format(bot), ' - ', bot.user.id)
-
-	while True:
-
-		statusNum = random.randint(0, len(playingStatus) - 1)
-		await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=playingStatus[statusNum]))
-
-		await asyncio.sleep(10)
-
-
-@bot.command(brief='Checks whos online',description='Checks whos online')
-async def online(ctx):
-
-	async with ctx.typing():
-
-		users_playing = {}
-		users_in_menu = []
-
-		for user_id in users: # adds user info if they are on a server in tf2 to users_playing
-
-			r = requests.get(f"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={os.getenv('steamtoken')}&steamids={discord2steam[user_id]}")
-			status = json.loads(r.text)['response']['players'][0]
-
-			try:
-
-				if status['gameid'] == '440':
-
-					try:
+# 				hours_played = json.loads(r.text)['response']['games'][0]['playtime_forever']/60
 				
-						users_playing[status['personaname']] = status['gameserverip'] # eg. {'Generic':'64.94.100.34:27015'}
+# 				# print(user_id + ': ' + discord2steam[user_id] + ': ' + str(hours_played))
 
-					except KeyError: # not in a server
+# 				try:
 
-						users_in_menu.append(status['personaname'])
-
-			except KeyError: # ignore if not playing tf2
-
-				pass
-
-		if len(users_playing) == 0 and len(users_in_menu) == 0: # no user online
-
-			embed=discord.Embed(title="no one is in a server rn :(", color=0xcf7336)
-
-		else:
-
-			server_ips = {} # groups users by server ip theyre connected to
-
-			for i, j in users_playing.items(): # shamelessly stolen from Stack Overflow lol
-
-				server_ips[j] = [i] if j not in server_ips.keys() else server_ips[j] + [i]
-
-			embed=discord.Embed(title="People Currently Playing TF2", color=0xcf7336)
-
-
-			if len(users_in_menu) != 0:
-
-				connected = ""
-
-				for player in users_in_menu:
+# 					if (hours_played // 100) > (hours[user_id] // 100):
 					
-					connected += ("> **" + player + "**\n")
+# 						embed=discord.Embed(title="Congratulations!", description=f"<@{user_id}> has reached **{str(int((hours_played // 100)*100))} hours** in TF2!", color=0xcf7336)
+
+# 						embed.timestamp = datetime.datetime.utcnow()
+
+# 						await bot.get_channel(890820026800676894).send(embed=embed)
 				
-				connected = connected.strip()
+# 				except KeyError: # New entry in dict
 
-				embed.add_field(name="In Menu", value=connected, inline=False)
+# 					pass
+
+# 				hours[user_id] = hours_played
+
+# 			except KeyError:
+
+# 				pass
+
+# 		print('hours updated')
+
+# 		with open("jsons/hours.json", 'w') as f:
+
+# 			json.dump(hours, f, indent=4)
+# 			f.close()
+
+# 		with open("jsons/hours.json", 'r') as f:
+			
+# 			hours = json.load(f)
+# 			f.close()
+			
+
+# 		await asyncio.sleep(300) # checks every 5 minutes
+
+# # async def vendor_period():
+
+# # 	await bot.wait_until_ready()
+# # 	while not bot.is_closed():
+
+# # 		await bot.get_channel(920551455868477460).send(". - randomdotvendor")
+# # 		await asyncio.sleep(random.randint(1,30)) # checks every 5 minutes
 
 
-			for ip_port in list(server_ips):
+# playingStatus = ['Ultiduos', 'Spire MGE', 'Uncletopia | Atlanta 1', '24/7 plr_hightower'
+# 				 ]
 
-				ip, port = ip_port.split(":")
 
-				r = requests.get(f"https://teamwork.tf/api/v1/quickplay/server?ip={ip}&port={port}&key={os.getenv('teamworktoken')}")
+# users = list(discord2steam)
 
-				try:
 
-					server = json.loads(r.text)[0]
+# @bot.event
+# async def on_ready():
 
-					connected = ""
+# 	print('Logged in as {0.user}'.format(bot), ' - ', bot.user.id)
 
-					for player in server_ips[ip_port]:
-						
-						connected += ("> **" + player + "**\n")
+# 	while True:
+
+# 		statusNum = random.randint(0, len(playingStatus) - 1)
+# 		await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=playingStatus[statusNum]))
+
+# 		await asyncio.sleep(10)
+
+
+# @bot.command(brief='Checks whos online',description='Checks whos online')
+# async def online(ctx):
+
+# 	async with ctx.typing():
+
+# 		users_playing = {}
+# 		users_in_menu = []
+
+# 		for user_id in users: # adds user info if they are on a server in tf2 to users_playing
+
+# 			r = requests.get(f"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={os.getenv('steamtoken')}&steamids={discord2steam[user_id]}")
+# 			status = json.loads(r.text)['response']['players'][0]
+
+# 			try:
+
+# 				if status['gameid'] == '440':
+
+# 					try:
+				
+# 						users_playing[status['personaname']] = status['gameserverip'] # eg. {'Generic':'64.94.100.34:27015'}
+
+# 					except KeyError: # not in a server
+
+# 						users_in_menu.append(status['personaname'])
+
+# 			except KeyError: # ignore if not playing tf2
+
+# 				pass
+
+# 		if len(users_playing) == 0 and len(users_in_menu) == 0: # no user online
+
+# 			embed=discord.Embed(title="no one is in a server rn :(", color=0xcf7336)
+
+# 		else:
+
+# 			server_ips = {} # groups users by server ip theyre connected to
+
+# 			for i, j in users_playing.items(): # shamelessly stolen from Stack Overflow lol
+
+# 				server_ips[j] = [i] if j not in server_ips.keys() else server_ips[j] + [i]
+
+# 			embed=discord.Embed(title="People Currently Playing TF2", color=0xcf7336)
+
+
+# 			if len(users_in_menu) != 0:
+
+# 				connected = ""
+
+# 				for player in users_in_menu:
 					
-					connected = connected.strip()
+# 					connected += ("> **" + player + "**\n")
+				
+# 				connected = connected.strip()
 
-					embed.add_field(name="Server Name", value=server['name'], inline=True)
-					embed.add_field(name="IP", value=ip_port, inline=True)
-					embed.add_field(name="Join this server", value=f"steam://connect/{ip_port}", inline=True)
+# 				embed.add_field(name="In Menu", value=connected, inline=False)
 
-					embed.add_field(name="Playing on this server", value=connected, inline=False)
+
+# 			for ip_port in list(server_ips):
+
+# 				ip, port = ip_port.split(":")
+
+# 				r = requests.get(f"https://teamwork.tf/api/v1/quickplay/server?ip={ip}&port={port}&key={os.getenv('teamworktoken')}")
+
+# 				try:
+
+# 					server = json.loads(r.text)[0]
+
+# 					connected = ""
+
+# 					for player in server_ips[ip_port]:
+						
+# 						connected += ("> **" + player + "**\n")
+					
+# 					connected = connected.strip()
+
+# 					embed.add_field(name="Server Name", value=server['name'], inline=True)
+# 					embed.add_field(name="IP", value=ip_port, inline=True)
+# 					embed.add_field(name="Join this server", value=f"steam://connect/{ip_port}", inline=True)
+
+# 					embed.add_field(name="Playing on this server", value=connected, inline=False)
 					
 
-				except (IndexError, KeyError) as e: # server not community or not on teamwork.tf list
+# 				except (IndexError, KeyError) as e: # server not community or not on teamwork.tf list
 
-					connected = ""
+# 					connected = ""
 
-					for player in server_ips[ip_port]:
+# 					for player in server_ips[ip_port]:
 						
-						connected += ("> **" + player + "**\n")
+# 						connected += ("> **" + player + "**\n")
 
-					connected = connected.strip()
+# 					connected = connected.strip()
 
-					embed.add_field(name="Server Name", value='Unknown (Valve or private server not on teamwork.tf)', inline=True)
-					embed.add_field(name="IP", value=ip_port, inline=True)
-					embed.add_field(name="Join this server", value=f"steam://connect/{ip_port}", inline=True)
+# 					embed.add_field(name="Server Name", value='Unknown (Valve or private server not on teamwork.tf)', inline=True)
+# 					embed.add_field(name="IP", value=ip_port, inline=True)
+# 					embed.add_field(name="Join this server", value=f"steam://connect/{ip_port}", inline=True)
 
-					embed.add_field(name="Playing on this server", value=connected, inline=False)
+# 					embed.add_field(name="Playing on this server", value=connected, inline=False)
 
 			
-		embed.set_footer(text=((f'Requested by {ctx.message.author.display_name} (') + str(ctx.message.author.id) + ')'))
-		embed.timestamp = datetime.datetime.utcnow()
+# 		embed.set_footer(text=((f'Requested by {ctx.message.author.display_name} (') + str(ctx.message.author.id) + ')'))
+# 		embed.timestamp = datetime.datetime.utcnow()
 
 
-	# embed.add_field(name='Activity Details', value=str(member.activity.details), inline=False)
-	await ctx.send(embed=embed)
+# 	# embed.add_field(name='Activity Details', value=str(member.activity.details), inline=False)
+# 	await ctx.send(embed=embed)
 
 
-@bot.command(brief='Gets recent logs from logs.tf',description='Gets recent logs from logs.tf')
-async def logs(ctx, member: discord.Member=None):
+# @bot.command(brief='Gets recent logs from logs.tf',description='Gets recent logs from logs.tf')
+# async def logs(ctx, member: discord.Member=None):
 
-	if member is None:
+# 	if member is None:
 
-		member = ctx.message.author
+# 		member = ctx.message.author
 	
 
-	async with ctx.typing():
+# 	async with ctx.typing():
 
 	
-		if str(member.id) not in list(discord2steam):
+# 		if str(member.id) not in list(discord2steam):
 
-			embed = discord.Embed(title="Sorry your SteamID isn't in the database yet :(", description="Contact <@538921994645798915> to tell them your SteamID", color=0xcf7336)
-
-
-		else:
-
-			r = requests.get(f"https://logs.tf/api/v1/log?player={discord2steam[str(member.id)]}&limit=5")
-
-			embed=discord.Embed(title='Recent logs of ' + member.display_name, url=f"https://logs.tf/profile/{discord2steam[str(member.id)]}", color=0xcf7336)
-
-			if len(json.loads(r.text)['logs']) == 0:
-
-				embed.description = "You don't have any logs on logs.tf :("
+# 			embed = discord.Embed(title="Sorry your SteamID isn't in the database yet :(", description="Contact <@538921994645798915> to tell them your SteamID", color=0xcf7336)
 
 
-			for log in json.loads(r.text)['logs']:
+# 		else:
 
-				embed.add_field(name=f"{log['title']} (https://logs.tf/{log['id']})", value=f"**{log['map']}**", inline=True)
-				embed.add_field(name='Date', value=datetime.datetime.fromtimestamp(log['date']).strftime('%Y-%m-%d'), inline=True)
-				embed.add_field(name='Players', value=log['players'], inline=True)
+# 			r = requests.get(f"https://logs.tf/api/v1/log?player={discord2steam[str(member.id)]}&limit=5")
 
-		embed.set_footer(text=((f'Requested by {ctx.message.author.display_name} (') + str(ctx.message.author.id) + ')'))
-		embed.timestamp = datetime.datetime.utcnow()
+# 			embed=discord.Embed(title='Recent logs of ' + member.display_name, url=f"https://logs.tf/profile/{discord2steam[str(member.id)]}", color=0xcf7336)
 
-	await ctx.send(embed=embed)
+# 			if len(json.loads(r.text)['logs']) == 0:
+
+# 				embed.description = "You don't have any logs on logs.tf :("
+
+
+# 			for log in json.loads(r.text)['logs']:
+
+# 				embed.add_field(name=f"{log['title']} (https://logs.tf/{log['id']})", value=f"**{log['map']}**", inline=True)
+# 				embed.add_field(name='Date', value=datetime.datetime.fromtimestamp(log['date']).strftime('%Y-%m-%d'), inline=True)
+# 				embed.add_field(name='Players', value=log['players'], inline=True)
+
+# 		embed.set_footer(text=((f'Requested by {ctx.message.author.display_name} (') + str(ctx.message.author.id) + ')'))
+# 		embed.timestamp = datetime.datetime.utcnow()
+
+# 	await ctx.send(embed=embed)
 
 
 @bot.command(brief='Medic performance analysis for logs',description='Medic performance analysis for logs from logs.tf')
@@ -383,24 +383,24 @@ async def medicstats(ctx, logtf_id):
 	await ctx.send(embed=embed)
 	
 
-@bot.command(brief='Playtime Leaderboard',description='Playtime Leaderboard')
-async def leaderboard(ctx):
+# @bot.command(brief='Playtime Leaderboard',description='Playtime Leaderboard')
+# async def leaderboard(ctx):
 
-	leaderboard = sorted(list(hours.items()),key=lambda x: x[1],reverse=True)[:10]
+# 	leaderboard = sorted(list(hours.items()),key=lambda x: x[1],reverse=True)[:10]
 
-	embed = discord.Embed(title="Playtime Leaderboard (hours)", color=0xcf7336)
+# 	embed = discord.Embed(title="Playtime Leaderboard (hours)", color=0xcf7336)
 
-	leaderboard_str = ''
+# 	leaderboard_str = ''
 
-	for i in range(1, 11):
+# 	for i in range(1, 11):
 
-		member = ctx.guild.get_member(int(leaderboard[i-1][0]))
+# 		member = ctx.guild.get_member(int(leaderboard[i-1][0]))
 
-		leaderboard_str += f"{str(i)}) **{member.display_name}** : {str(round(leaderboard[i-1][1],1))}\n\n"
+# 		leaderboard_str += f"{str(i)}) **{member.display_name}** : {str(round(leaderboard[i-1][1],1))}\n\n"
 	
-	embed.description = leaderboard_str
+# 	embed.description = leaderboard_str
 
-	await ctx.send(embed=embed)
+# 	await ctx.send(embed=embed)
 		
 
 @bot.command(brief='Returns user info',description='Returns user info')
@@ -413,23 +413,23 @@ async def user(ctx, member: discord.Member=None):
 	embed.add_field(name='Username', value=member, inline=False)
 	embed.add_field(name='Server Nickname', value=member.nick, inline=False)
 
-	try:
-		embed.add_field(name='Steam', value=f"https://steamcommunity.com/profiles/{discord2steam[str(member.id)]}", inline=False)
-	except KeyError:
-		pass
-	try: 
-		embed.add_field(name='Hours in TF2', value=str(round(hours[str(member.id)])), inline=False)
-	except KeyError:
-		pass
-	try:
-		embed.add_field(name='RGL', value=f"https://rgl.gg/Public/PlayerProfile.aspx?p={discord2steam[str(member.id)]}", inline=False)
-		embed.add_field(name='UGC', value=f"https://www.ugcleague.com/players_page.cfm?player_id={discord2steam[str(member.id)]}", inline=False)
-	except KeyError:
-		pass
-	# embed.add_field(name='ID', value='`'+str(member.id)+'`', inline=False)
-	# embed.add_field(name='Status', value='`'+str(member.status)+'`', inline=False)
-	# embed.add_field(name='Joined', value='`'+str(member.joined_at)+'`', inline=False)
-	embed.add_field(name='Role', value=str(member.top_role), inline=False)
+	# try:
+	# 	embed.add_field(name='Steam', value=f"https://steamcommunity.com/profiles/{discord2steam[str(member.id)]}", inline=False)
+	# except KeyError:
+	# 	pass
+	# try: 
+	# 	embed.add_field(name='Hours in TF2', value=str(round(hours[str(member.id)])), inline=False)
+	# except KeyError:
+	# 	pass
+	# try:
+	# 	embed.add_field(name='RGL', value=f"https://rgl.gg/Public/PlayerProfile.aspx?p={discord2steam[str(member.id)]}", inline=False)
+	# 	embed.add_field(name='UGC', value=f"https://www.ugcleague.com/players_page.cfm?player_id={discord2steam[str(member.id)]}", inline=False)
+	# except KeyError:
+	# 	pass
+	# # embed.add_field(name='ID', value='`'+str(member.id)+'`', inline=False)
+	# # embed.add_field(name='Status', value='`'+str(member.status)+'`', inline=False)
+	# # embed.add_field(name='Joined', value='`'+str(member.joined_at)+'`', inline=False)
+	# embed.add_field(name='Role', value=str(member.top_role), inline=False)
 		
 	embed.set_footer(text=((f'Requested by {ctx.message.author.display_name} (') + str(ctx.message.author.id) + ')'))
 	embed.timestamp = datetime.datetime.utcnow()
@@ -596,7 +596,7 @@ t1 = time.time()
 
 bot.loop.create_task(heartbeat())
 
-bot.loop.create_task(check_hours_played())
+# bot.loop.create_task(check_hours_played())
 
 # bot.loop.create_task(vendor_period())
 
